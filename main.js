8bc402445be2091e9b84f16aab7ff76cfd42ed10
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-08-21 08:29:31 +0600

    Add files via upload
    Variables var,let,const when?

---
+++ main.js
@@ -0,0 +1,190 @@
+
+
+/*
+Variables var,let,const when?
+
+Node:1
+let name,age,country;
+
+
+name = "Mamun Khan";
+
+age = 48;
+
+country = "Bangladesh";
+
+
+document.write(name);
+document.write(age);
+document.write(country);
+
+var city = "Dhaka";
+var city = "Gaibandha";
+
+document.write(city);
+console.log(city);
+
+const web = "Dhaka";
+
+document.write(web);
+*/
+
+
+/*
+Node:2
+let name = "Mamun Khan";
+
+document.write(name);
+console.log(name);
+
+if(true){
+
+              name = ("Robin");
+
+              document.write("In side Tha Block");
+              console.log("in side Tha Block");
+              document.write(name);
+              console.log(name);
+}
+
+document.write(name);
+
+document.write("Out side Tha Block");
+console.log("Out side Tha Block");
+console.log(name);
+*/
+
+/*
+Node:3
+
+if(true){
+              var name = "Mamun";
+}
+
+console.log(name);
+
+if(true){
+              let name_1 = "Mamun Khan";
+
+              console.log(name_1);
+}
+
+*/
+
+/*
+Node:3
+
+if(true){
+              var name = "Mamun";
+              console.log("Robin");
+}
+
+console.log(name);
+
+if(true){
+              let name_1 = "Mamun Khan";
+
+              console.log(name_1);
+}
+
+*/
+
+/*
+Node:4
+
+var msg = "How are you";
+
+function sayHai(){
+
+              console.log(msg);
+}
+
+sayHai();
+*/
+
+
+/*
+Node:5
+
+var msg = "How are you";
+
+function sayHai(msg){
+
+              console.log(msg);
+}
+
+sayHai("vary Good");
+*/
+
+/*
+Node:6
+var msg = "How are you";
+
+function sayHai(msg){
+
+              console.log(msg);
+}
+
+sayHai("vary Good");
+
+*/
+
+/*
+Node:7
+
+function jogKoro(){
+
+              let x = 10;
+
+              let y = 10;
+
+function add(){
+              return (x + y) ;
+}
+console.log(add());
+
+}
+
+jogKoro();
+*/
+
+
+
+/*
+Node:8
+const FirstName = "Md";
+
+const LastName ="Rahman";
+
+console.log(FirstName,LastName);
+
+*/
+
+/*
+Node:9
+
+const FirstName = "Md";
+
+const LastName ="Rahman";
+
+function fullName() {
+
+              console.log(FirstName,LastName);
+}
+fullName();
+
+*/
+
+/*
+Node:9
+
+const FirstName = "Md";
+
+const LastName ="Rahman";
+
+function fullName() {
+
+              console.log(FirstName,LastName);
+}
+fullName();
+*/
